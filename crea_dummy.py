e3509a946448a1d9d5f29116647aaf9d76b13db0
rmarzocchi84 <roberto.marzocchi@gter.it>
2020-08-12 09:07:54 +0000

    Integrate modifiche debug (GTER) e Mussin per il ricalcolo

--- crea_dummy.py
+++ crea_dummy.py
@@ -39,12 +39,18 @@ import getopt
 space = re.compile(r'\s+')
 multiSpace = re.compile(r"\s\s+")
 
+# debug
+# 0 = only short message (Quiet mode) 
+# 1 = a lot of print message (Verbose mode) 
+debug=0
+
 
 
 def checkpathfile(time_steps,path,title_file,data_read,utc,file_format,interval):
     i=0
     data_check=data_read
-    print "Start checking {0}_ files existing".format(title_file)
+    if debug==1:
+        print("Start checking {0}_ files existing".format(title_file))
     while i < int(time_steps):
         if i>0:
             data_check=data_check -datetime.timedelta(hours=int(interval))
@@ -57,7 +63,7 @@ def checkpathfile(time_steps,path,title_file,data_read,utc,file_format,interval)
         else:
             namefile_check="{0}/{1}_{2}UTC{3}.{4}".format(path,title_file,data_file_new,utc,file_format)
         if os.path.isfile(namefile_check)==False:
-            print "ERROR: file {0} doesn't exist".format(namefile_check)
+            print("ERROR: file {0} doesn't exist".format(namefile_check))
             sys.exit() 
             # directory exists
         i+=1
@@ -124,7 +130,8 @@ def main():
         opts, args = getopt.getopt(sys.argv[1:], "ms:t:h:p:", ["help", "start_time=", "time_steps=", "interval=","prefix"])
     except getopt.GetoptError:
         manual= 'ascii2grids.py -s <start time wirth the format of ascii file> -t <TDEF number of time to consider> -h <TDEF interval in hours> -p <prefix of file>'
-        print manual
+        if debug==1:
+            print(manual)
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-m':
@@ -139,23 +146,24 @@ def main():
         elif opt in ("-p", "--interval"):
             prefix = arg
     if start_time=='':
-        print 'ERROR: specify a start time'
+        print('ERROR: specify a start time')
         sys.exit()
     if time_steps=='':
-        print 'ERROR: specify a time step (also 1 is allowed)'
+        print('ERROR: specify a time step (also 1 is allowed)')
         sys.exit()
     if interval=='':
-        print 'ERROR: specify the time interval (e.g hours=1)'
+        print('ERROR: specify the time interval (e.g hours=1)')
         sys.exit()
     if prefix=='':
-        print 'ERROR: specify a prefix for dummy file '
+        print('ERROR: specify a prefix for dummy file ')
         sys.exit()        
-            
-    print 'Time start is ', start_time
-    print 'Time step: ', time_steps
-    print 'Hour Interval: ', interval    
+    
     file_time= start_time
-    print file_time
+    if debug==1:        
+        print('Time start is ', start_time)
+        print('Time step: ', time_steps)
+        print('Hour Interval: ', interval)    
+        print(file_time)
     data_file=file_time.split('.')
     file_format=data_file[1]
     data_temp=data_file[0].split('UTC')
@@ -163,12 +171,13 @@ def main():
     
     
     data_file=data_temp[0]
-    
-    print data_file
+    if debug==1:
+        print(data_file)
     #data_read=time.strptime(data_file,"%Y%m%d%H")
     #print data_read
     data_read=datetime.datetime.strptime(data_file,"%Y%m%d%H")
-    print data_read
+    if debug==1:
+        print(data_read)
     
     #strptime("30 Nov 00", "%d %b %y")
     #data_read=data_file.strftime("%Y%m%d%H")
@@ -183,14 +192,15 @@ def main():
     utc=data_temp[1]
     sign_temp=utc.split('s')
     sign_check=len(sign_temp[0])
-    print data_temp[1].split('s')
-    print sign_temp[1]
+    if debug==1:
+        print(data_temp[1].split('s'))
+        print(sign_temp[1])
     if (sign_check==3):
         utc_zone=int(sign_temp[1])
     elif (sign_check==4):
         utc_zone=int(sign_temp[1])*-1
     else:
-        print 'ERROR: invalid UTC time'
+        print('ERROR: invalid UTC time')
         sys.exit()     
  
     # calcolo data_write (sbagliato.. interpretato male i dati e scrivevamo come data_write quella finale, invece nel file ctl ci vuole la data iniziale)
@@ -204,7 +214,8 @@ def main():
     
     i=0
     data_check=data_read
-    print "Start creating files".format(prefix)
+    if debug==1:
+        print("Start creating files {0}".format(prefix))
     while i < int(time_steps):
         if i>0:
             data_check=data_check -datetime.timedelta(hours=int(interval))
@@ -213,7 +224,8 @@ def main():
         #print data_check
         data_file_new=data_check.strftime("%Y%m%d%H")
         namefile_check="{4}/{0}_{1}UTC{2}.{3}".format(prefix,data_file_new,utc,file_format,path_mediano)
-        print namefile_check
+        if debug==1:
+            print(namefile_check)
         # deve sempre esserci il file con tutti i nulli con nome prefissato dummy_null.txt
         copyfile('{0}/DUMMY_NOT_REMOVE.txt'.format(path_mediano), namefile_check)
         i+=1
